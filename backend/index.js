c6c2eec4bda884a0ac1e5c32752e9cd94d815d60
d-najd <dimitar.najdovskiw@gmail.com>
2026-04-08 15:41:54 +0200

    init

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+	res.json({ message: 'API working' })
+})
+
+app.listen(3000, () => console.log('Server running'))
